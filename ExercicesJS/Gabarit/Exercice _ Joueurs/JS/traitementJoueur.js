850bf1402bd9ddcc31b3f9478e3875e2d55b5fbb
PAYTH1830180 <Forumposts1108>
2018-11-15 09:53:28 -0500

    Finnision Joueur

--- ExercicesJS/Gabarit/Exercice _ Joueurs/JS/traitementJoueur.js
+++ ExercicesJS/Gabarit/Exercice _ Joueurs/JS/traitementJoueur.js
@@ -1,4 +1,4 @@
-var NonJoueur, NbPoint, cpt = 0, saisi, points;
+var NonJoueur, NbPoint, cpt = 0, saisi, points, moyen;
 var TabJoueur = new Array(4);
 var TabPoints = new Array(4);
 
@@ -36,6 +36,43 @@ function btnTrouverMoy_onclick()
     moyen = tot/4;
     document.getElementById("lblReponse").innerHTML= ("Reponse " + moyen);
 }
+
+function btnTrouverPire_onclick()
+
+{
+    var min = TabPoints[0];
+    var nomPire = TabJoueur[0];
+    for(var i = 0; i < TabPoints.length; i ++)
+    {
+        if(min > TabPoints[i])
+        {
+            min = TabPoints[i];
+            nomPire = TabJoueur[i];
+        }
+
+    }
+    document.getElementById("lblReponse").innerHTML= "Reponse " + min + " Joueur " + nomPire;
+}
+
+function btnRechercher_onclick()
+{
+    cpt = 0;
+    var recherche = TabJoueur[0];
+    var score = TabPoints[0];
+    do
+    {
+        cpt = -1;
+        cpt++;
+
+
+        if (recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt])
+        {
+            document.getElementById("lblReponse").innerHTML = "Reponse " + score + " Joueur " + recherche;
+        }
+    }while(recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt])
+
+}
+
 
 
 
